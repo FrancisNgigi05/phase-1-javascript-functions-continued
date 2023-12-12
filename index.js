6dbfd84ee9afafe8dfe0012a1f4fe4ce9542fdcd
FrancisNgugi05 <francis.ngugi@student.moringaschool.com>
2023-12-12 22:02:02 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun (activity = "roller-skate") {
+    return (`This Saturday, I want to ${activity}!`);
+}
+
+let fun = saturdayFun();
+console.log(fun);
+fun = saturdayFun("bathe my dog")
+console.log(fun);
+
+const mondayWork = function (activity = 'go to the office') {
+    return (`This Monday, I will ${activity}.`);
+}
+
+let mondayActivity = mondayWork();
+
+function wrapAdjective(symbol = "*") {
+    const innerFunction = function(word = "special") {
+        return (`You are ${symbol}${word}${symbol}!`);
+    }
+    return innerFunction;
+}
+
+wrapAdjective("%") ("a dedicated programmer")
